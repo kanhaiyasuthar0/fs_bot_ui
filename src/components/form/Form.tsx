0f41138549d6c81caf6c401e7d319a022d182d9e
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-07-08 16:53:24 +0530

    form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import "./FeedbackForm.css";
 import { Button, Input, Select } from "antd";
 import TextArea from "antd/es/input/TextArea";
+import { CSSTransition } from "react-transition-group";
+import { Rating, Typography } from "@mui/material";
+// import Rating from "react-rating";
 // const mongoose = require("mongoose");
 
 // Connect to MongoDB
@@ -23,11 +26,17 @@ import TextArea from "antd/es/input/TextArea";
 // Create a feedback model
 // const Feedback = mongoose.model("Feedback", feedbackSchema);
 const Form = (props) => {
+  const [value, setValue] = React.useState(2);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [rating, setRating] = useState("");
   const [description, setDescription] = useState("");
   const [mobileNumber, setMobileNumber] = useState();
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNextStep = (value) => {
+    setActiveStep((prev) => prev + value);
+  };
 
   const handleSubmit = async (event) => {
     if (event) {
@@ -37,7 +46,7 @@ const Form = (props) => {
     // You can send the feedback data to a server or perform any other actions
 
     // Clear form inputs
-    console.log(props.tele);
+    console.log(props.tele, "INSIDE THE HANDLE SUBMIT");
     console.log(name, email, rating, description, mobileNumber);
     // props.tele.initData = "initData random";
     // props.tele.initDataUnsafe["random"] = "initData random";
@@ -45,7 +54,7 @@ const Form = (props) => {
       JSON.stringify({
         name,
         email,
-        rating,
+        rating: value,
         description,
         phone_number: mobileNumber,
       })
@@ -63,14 +72,15 @@ const Form = (props) => {
   const mainButton = props.tele.MainButton;
   mainButton.text = "Submit feedback";
   mainButton.color = "#00ab55";
-  props.tele.onEvent("mainButtonClicked", (e) => {
+  props.tele.onEvent("mainButtonClicked", (e: any) => {
+    console.log("reading");
     handleSubmit(e);
   });
   const checkAllField = () => {
     if (name && email && rating && description && mobileNumber) {
       mainButton.enable();
       mainButton.show();
-      // mainButton.onClick(handleSubmit);
+      mainButton.onClick(() => handleSubmit);
     } else {
       mainButton.disable();
       mainButton.hide();
@@ -89,100 +99,201 @@ const Form = (props) => {
     <div className="feedback-form-container">
       <h2>Feedback Form</h2>
       {/* <form onSubmit={handleSubmit}> */}
-      <div className="form-group">
-        {/* <label htmlFor="name">Name</label> */}
-        <Input
-          placeholder="Name..."
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-
-      <div className="form-group">
-        {/* <label htmlFor="email">Email</label> */}
-        <Input
-          placeholder="Email..."
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-
-      <div
-        className="form-group"
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-evenly",
+      <CSSTransition
+        in={activeStep === 0}
+        timeout={{
+          appear: 1000,
+          enter: 900,
+          exit: 300,
         }}
+        classNames="step"
+        unmountOnExit
       >
-        <label htmlFor="rating">Rating</label>
-        <Select
-          size="large"
-          placeholder="Rating..."
-          style={selectStyle}
-          id="rating"
-          value={rating}
-          onChange={(e) => setRating(e)}
-          required
-          options={[
-            // { value: "", label: "Rating" },
-            { value: "good", label: "Good" },
-            { value: "average", label: "Average" },
-            { value: "bad", label: "Bad" },
-          ]}
-        />
-        {/* <option value=""></option>
+        <>
+          <div className="form-group">
+            {/* <label htmlFor="name">Name</label> */}
+            <Input
+              placeholder="Name..."
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          {/* </CSSTransition>
+      <CSSTransition
+        in={activeStep === 0}
+        timeout={{
+          appear: 600,
+          enter: 700,
+          exit: 100,
+        }}
+        classNames="step"
+        unmountOnExit
+      > */}
+          <div className="form-group">
+            {/* <label htmlFor="email">Email</label> */}
+            <Input
+              placeholder="Email..."
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            // style={{
+            disable={name && email ? "false" : "true"}
+            // }}
+            type="submit"
+            className="button-46"
+            onClick={activeStep === 1 ? handleSubmit : () => handleNextStep(1)}
+          >
+            {activeStep === 1 ? "Submit" : "Next"}
+          </button>
+        </>
+      </CSSTransition>
+      <CSSTransition
+        // appear={activeStep === 1}
+        in={activeStep === 1}
+        timeout={{
+          appear: 1000,
+          enter: 900,
+          exit: 300,
+        }}
+        classNames="step"
+        unmountOnExit
+      >
+        <>
+          <div
+            className="form-group"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <label style={{ color: "white" }} component="rating">
+              Rating
+            </label>
+            <div
+              style={{
+                background: "green",
+                color: "black",
+                borderRadius: "10px",
+                padding: "10px",
+              }}
+            >
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </div>
+            {/* <Select
+            size="large"
+            placeholder="Rating..."
+            style={selectStyle}
+            id="rating"
+            value={rating}
+            onChange={(e) => setRating(e)}
+            required
+            options={[
+              // { value: "", label: "Rating" },
+              { value: "good", label: "Good" },
+              { value: "average", label: "Average" },
+              { value: "bad", label: "Bad" },
+            ]}
+          /> */}
+            {/* <option value=""></option>
           <option value="good">Good</option>
           <option value="bad">Bad</option>
           <option value="average">Average</option> */}
-        {/* </Select> */}
-      </div>
+            {/* </Select> */}
+          </div>
+          {/* </CSSTransition>
 
-      <div className="form-group">
-        <label htmlFor="description">Tell us what can be improved?</label>
-        <TextArea
-          name="desc"
-          placeholder="Describe..."
-          rows={6}
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-      </div>
-
-      <div className="form-group">
-        {/* <label htmlFor="mobileNumber">Mobile Number</label> */}
-        <Input
-          placeholder="Mobile number"
-          type="tel"
-          id="mobileNumber"
-          value={disabledMobileNumber}
-          // onChange={(e) => setMobileNumber(e.target.value)}
-          contentEditable={false}
-          required
-        />
-      </div>
-
-      <button
-        style={{
-          display:
-            name && description && rating && mobileNumber && email
-              ? "true"
-              : "false",
-          backgroundColor: "#00ab55",
+      <CSSTransition
+        in={activeStep === 1}
+        timeout={{
+          appear: 600,
+          enter: 700,
+          exit: 100,
         }}
-        type="submit"
-        onClick={handleSubmit}
-      >
-        Submit
-      </button>
+        classNames="step"
+        unmountOnExit
+      > */}
+          <div className="form-group">
+            <label htmlFor="description">Tell us what can be improved?</label>
+            <TextArea
+              name="desc"
+              placeholder="Describe..."
+              rows={6}
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </div>
+          {/* </CSSTransition>
+      <CSSTransition
+        in={activeStep === 1}
+        timeout={{
+          appear: 600,
+          enter: 700,
+          exit: 100,
+        }}
+        classNames="step"
+        unmountOnExit
+      > */}
+          <div className="form-group">
+            {/* <label htmlFor="mobileNumber">Mobile Number</label> */}
+            <Input
+              placeholder="Mobile number"
+              type="tel"
+              id="mobileNumber"
+              value={disabledMobileNumber}
+              // onChange={(e) => setMobileNumber(e.target.value)}
+              contentEditable={false}
+              required
+            />
+          </div>
+          <button
+            // style={{
+            //   display:
+            //     name && description && rating && mobileNumber && email
+            //       ? "true"
+            //       : "false",
+
+            //   border: "1px solid #00ab55",
+            // }}
+            type="submit"
+            className="button-45"
+            onClick={activeStep === 1 ? () => handleNextStep(-1) : () => ""}
+          >
+            {activeStep === 1 ? "Previous" : "Next"}
+          </button>
+          <button
+            // style={{
+            //   display:
+            //     name && description && rating && mobileNumber && email
+            //       ? "true"
+            //       : "false",
+            //   backgroundColor: "#00ab55",
+            // }}
+            type="submit"
+            className="button-46"
+            onClick={activeStep === 1 ? handleSubmit : () => handleNextStep(1)}
+          >
+            {activeStep === 1 ? "Submit" : "Next"}
+          </button>
+        </>
+      </CSSTransition>
+
       {/* </form> */}
     </div>
   );
